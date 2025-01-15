19e9966852f492c2b93d2910baacc0a045de672b
Matthias Villiger <mvi@bsi-software.com>
2025-01-15 16:34:29 +0100

    DataObject support for Scout JS
    - Introduce a DataObjectTransformer which adds annotations during
      TypeScript compile time to all attributes of DataObjects. These
      annotations contain the data type of the attribute to be retrieved at
      runtime.
    - Add a base class for DataObjects: BaseDoEntity
    - Add a DataObjectInventory to retrieve all DataObjects known in TS
    - Migrate existing DOs to the new base class including decorators.
    - Add convenience methods to ajax utility to post and get DataObjects
    - Add a DataObject (De)Serializer for TS. It uses the data type
      annotations added by the compile time transformer and supports:
      - Arrays
      - Maps
      - Sets
      - Records
      - Dates
    - Enable experimental decorators for Scout JS
    - Migrate from deprecated 'type' jQuery ajax property to the new
      'method' property.
    - Add idCodec utility to de- and encode Ids from/to strings.
    - Adjust objects.equals, equalsRecursive & valueCopy. Changes:
      - Support for Date
      - Classes must be same for two objects to be equal
      - Support for Sets/Maps
    - Add objects.equalsSet & objects.equalsMap
    
    389583

--- eclipse-scout-core/src/form/js/JsFormAdapter.ts
+++ eclipse-scout-core/src/form/js/JsFormAdapter.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {ChildModelOf, Event, Form, FormAdapter, FullModelOf, JsFormModel, Widget} from '../../index';
+import {ChildModelOf, dataObjects, Event, Form, FormAdapter, FullModelOf, JsFormModel, Widget} from '../../index';
 
 export class JsFormAdapter extends FormAdapter {
 
@@ -24,11 +24,14 @@ export class JsFormAdapter extends FormAdapter {
       objectType: model.jsFormObjectType,
       displayParent: model.displayParent,
       displayHint: model.displayHint,
-      data: model.inputData
+      data: dataObjects.deserialize(model.inputData, null, {createPojoIfDoIsUnknown: true})
     };
 
     if (model.jsFormModel) {
-      jsFormModel = $.extend(true, {}, model.jsFormModel, jsFormModel);
+      delete model.jsFormModel.objectType; // objectType from Form model should not be used as model.jsFormObjectType specifies the object already
+      let deserializedJsFormModel = dataObjects.deserialize(model.jsFormModel, null, {createPojoIfDoIsUnknown: true});
+      delete deserializedJsFormModel._type; // _type should not be written to Form if present
+      jsFormModel = $.extend({}, deserializedJsFormModel, jsFormModel);
     }
 
     return jsFormModel;
@@ -94,7 +97,7 @@ export class JsFormAdapter extends FormAdapter {
 
   protected _sendOutputData(type: string, data?: any) {
     this._send(type, {
-      outputData: data
+      outputData: dataObjects.serialize(data)
     });
   }
 
